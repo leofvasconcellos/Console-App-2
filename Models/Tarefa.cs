a89eec93a27507bd96a5822d206320ddcf6c6551
Leandro Farias de Vasconcellos <leofvasconcellos@gmail.com>
2024-09-27 09:58:51 -0300

    projeto
    Subida do projeto.

---
+++ Models/Tarefa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Models
+{
+    internal class Tarefa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public Tarefa(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using ConsoleApp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Criando uma lista de tarefas
+            List<Tarefa> tarefas = new List<Tarefa>{
+            new Tarefa(1, "Tarefa Limpar"),
+            new Tarefa(2, "Tarefa Lavar"),
+            new Tarefa(3, "Tarefa Polir"),
+            new Tarefa(4, "Tarefa Colocar para venda"),
+            new Tarefa(5, "Tarefa Vender"),
+            new Tarefa(5, "Tarefa Entregar")
+            };
+
+            // Criando uma lista de tarefas para executar
+            List<Task> tarefasAsync = new List<Task>();
+
+            // Processando cada tarefa de forma assíncrona
+            foreach (var tarefa in tarefas)
+            {
+                tarefasAsync.Add(ProcessarTarefa(tarefa));
+            }
+
+            // Espera todas as tarefas serem concluídas
+            await Task.WhenAll(tarefasAsync);
+
+            // Mantendo a aplicação aberta, para visualização das informações
+            Console.WriteLine("Pressione Enter para sair...");
+            Console.ReadLine();
+        }
+
+        // Método que irá simular o processamento de cada tarefa
+        static async Task ProcessarTarefa(Tarefa tarefa)
+        {
+            // Marca o tempo de início
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // Simula um atraso aleatório para o processamento
+            Random random = new Random();
+            int tempoDeExecucao = random.Next(1000, 5000); // entre 1 e 5 segundos
+            await Task.Delay(tempoDeExecucao);
+
+            // Marca o tempo de término
+            stopwatch.Stop();
+
+            // Imprime no console os resultados
+            Console.WriteLine($"Tarefa ID: {tarefa.Id}, Nome: {tarefa.Nome}, Tempo de Execução: {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
